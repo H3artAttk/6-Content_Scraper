7d41e7a94d5afbc888636e76f990fe08c766210a
noahub <noah.matsell@unbounce.com>
2017-10-23 16:06:42 -0700

    udpated reqs

---
+++ scraper.js
@@ -0,0 +1,9 @@
+//Check for a folder ‘data’. If it exist, create one.
+//get the price, title, url and image url from the product page
+//Should be stored in an CSV file that is named for the date, e.g. 2016-11-21.csv.
+//Column headers in this order: Title, Price, ImageURL, URL, and Time
+//CSV file should be saved inside the ‘data’ folder.
+	//If your program is run twice, it should overwrite the data in the CSV file with the updated information.
+//log errors to a file named scraper-error.log
+	//should append to the bottom of the file with a time stamp and error 
+	//e.g. [Tue Feb 16 2016 10:02:12 GMT-0800 (PST)] <error message>
