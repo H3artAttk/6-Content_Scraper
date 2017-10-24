ab2bbdd2999c14c53e2aef5857c4487c813e63cf
noahub <noah.matsell@unbounce.com>
2017-10-23 17:35:48 -0700

    prelim work

--- scraper.js
+++ scraper.js
@@ -2,7 +2,28 @@ const csv = require('csv');
 const request = require('request');
 const cheerio = require('cheerio');
 //Check for a folder ‘data’. If it exist, create one.
+let url = "http://www.shirts4mike.com/shirts.php";
+
+//request html
+request(url, function(error, response, html){
+  if(!error){
+  	getProducts(html);
+  }
+});
+
+//parse out products from html
+function getProducts(html){
+	var $ = cheerio.load(html);
+	$('.products li').forEach(function(element) {
+		//get href from nested a in li
+	});
+};
+
 //get the price, title, url and image url from the product page
+function getShirtDetails(){
+	request();
+};
+
 //Should be stored in an CSV file that is named for the date, e.g. 2016-11-21.csv.
 //Column headers in this order: Title, Price, ImageURL, URL, and Time
 //CSV file should be saved inside the ‘data’ folder.
